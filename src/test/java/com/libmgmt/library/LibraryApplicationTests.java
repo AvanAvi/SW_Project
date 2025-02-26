f64ba3af9d191bef43e7aa54d33994ce03923d68
AvanAvi <theastrofreek@gmail.com>
2025-02-26 22:09:19 +0100

    Initial Spring Boot Project setup

---
+++ src/test/java/com/libmgmt/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libmgmt.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
